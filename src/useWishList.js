ca7a0937e201aa110e6553f9ef4a69dd881c48d2
AmarRalii <xblakmero264@gmail.com>
2024-06-19 22:25:58 +0300

    updated

--- src/useWishList.js
+++ src/useWishList.js
@@ -21,7 +21,7 @@ export function deleteWish(productId) {
         }
     })
 }
-//ecommerce.routemisr.com
+//ecommerce.routemisr.co
 
 export function getWish() {
     return axios.get(`https://ecommerce.routemisr.com/api/v1/wishlist`, {
